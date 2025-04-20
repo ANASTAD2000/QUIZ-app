0705acc1e090585cfb8415a22ff2fb826d38982f
ANASTAD2000 <anasbaclibr@gmai.com>
2025-04-20 21:32:02 +0100

    categoris is work

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { QuizService } from '../quiz.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink,FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink, HttpClientModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-selectedCategory: any;
+export class HomeComponent implements OnInit {
+  selectedCategory: any;
+  categories: any[] = [];
 
+  constructor(private quizService: QuizService) {}
+
+  ngOnInit(): void {
+    this.quizService.getCategories().subscribe((data: any) => {
+      this.categories = data.trivia_categories;
+    });
+  }
 }
